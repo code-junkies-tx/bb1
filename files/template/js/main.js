e83f9f468244044cceb087fbfc5773bec8e72499
Andy <andythomas.py@gmail.com>
2023-03-07 20:55:27 -0600

    Added js loop for bb images

---
+++ files/template/js/main.js
@@ -0,0 +1,8 @@
+let bbThreeImages = '' 
+
+const bbImage = '<img src="bb-img.jpeg" alt="bbImg">'
+
+for (let i = 0; i < 3; i++) {
+    bbThreeImages += bbImage
+    document.querySelector("#bbImages").innerHTML = bbThreeImages
+}
