b6ea133d8be61e410b0e07d5c7f7f461c5149361
Andy <andythomas.py@gmail.com>
2023-03-07 20:07:16 -0600

    Removed reduntatnt movie data code

--- files/template/js/main.js
+++
@@ -1,9 +0,0 @@
-const imgContainer = document.querySelector(".bbImages")
-
-const mainImage = '<img class="imgClass" src="../template/images/bb-img.jpeg" alt="bb-img">'
-
-
-
-for (let i = 0; i < 3; i++) {
-    imgContainer.insertAdjacentHTML("afterbegin" , mainImage);
-  }
